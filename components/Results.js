6e9d29166cb0c9e3d22b1da0c15e1bddb03b578b
Emmanuel Akala <adekyte@gmail.com>
2021-06-28 23:31:01 +0100

    finishing touches ...v1

--- components/Results.js
+++ components/Results.js
@@ -1,9 +1,13 @@
-const Results = ({ results }) => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import Thumbnail from './Thumbnail';
 
-export default Results
+const Results = ({ results }) => {
+  return (
+    <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
+      {results.map((result) => (
+        <Thumbnail key={result.id} result={result} />
+      ))}
+    </div>
+  );
+};
+
+export default Results;
